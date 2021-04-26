b30f4473e29ea74bc2d6d9442c498dc04710b928
fulviocanducci <fulviocanducci@hotmail.com>
2021-04-25 23:33:33 -0300

    phpmyadmin

--- src/index.php
+++ src/index.php
@@ -1,2 +1,13 @@
 <?php
-    phpinfo();
+    //phpinfo();
+
+    $pdo = new PDO("mysql:host=mysql;port=3306;dbname=test", "dev", "123456");
+
+    $stmt = $pdo->prepare("SELECT * FROM credit ORDER BY id ASC");
+    $stmt->execute();
+    $obj = $stmt->fetchAll();
+    foreach($obj as $o) 
+    {
+        echo $o['id'] . ' - ' . $o['description'];
+        echo '<br />';
+    }
